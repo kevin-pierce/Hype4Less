d2af5fc79f31423c86f4b5ac92a4977d3594a9ba
Kevin Pierce <kevinpierce08@gmail.com>
2021-01-08 12:19:42 -0500

    Initialized Navbar with bootstrap - no longer using TypeScript

---
+++ hype-4-less/src/components/APP/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import NavbarHeader from "../NAVBAR/index.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavbarHeader/>
+    </div>
+  );
+}
+
+export default App;
--- hype-4-less/src/components/HOMEPAGE/index.jsx
+++ hype-4-less/src/components/HOMEPAGE/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 
-const HomePage: React.FC = () => {
+const HomePage = () => {
     return(
         <div>
             <h1>DEALS OF THE DAY</h1>
