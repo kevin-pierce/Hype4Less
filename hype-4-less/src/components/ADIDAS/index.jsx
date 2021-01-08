bc955cc7638e8b6b451603b6d36fe16a26b8d6de
Kevin Pierce <kevinpierce08@gmail.com>
2021-01-08 14:59:00 -0500

    List of products now appears as intended for Adidas page

--- hype-4-less/src/components/ADIDAS/index.jsx
+++ hype-4-less/src/components/ADIDAS/index.jsx
@@ -4,31 +4,54 @@ import axios from "axios";
 
 const AdidasProdPage = () => {
     const [prodData, setProdData] = useState({ products: []});
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
+
+            setIsLoading(true);
             const allData = await axios('http://127.0.0.1:5000/shoepic/api/prod/v1.0/sales/tiro/');
             
-            console.log(allData.data.adidasData)
             setProdData(allData.data.adidasData);
+            setIsLoading(false);
         };
         fetchData();
     }, []);
-
-    console.log(prodData);
     return (
         <div>
-            <ul>
-                {prodData.map(item => (
-                    <li key={item["prodName"]}>
-                        <a href={item["prodLink"]}>{item["prodName"]}</a>
-                    </li>
-                ))}
-            </ul>
+
+            {isLoading ? (
+                <h1>Is loading...</h1>
+            ) 
+            : 
+            (
+                <ul>
+                    {prodData.map(item => (
+                        <li>
+                            <a href={item["prodLink"]}>{item["prodName"]}</a>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            
         </div>
     );
 }
 
 export default AdidasProdPage;
 
-            
+//<a href={item["prodLink"]}>{item["prodName"]}</a>
+// {prodData.map(item => (
+//     <li>
+//         <p>LOL</p>
+        
+//     </li>
+// ))}
+
+
+
+// {prodData.products(item => (
+//     <li>
+//         <a href={item["prodLink"]}></a>
+//     </li>
+// ))}
