7314cc9473d4a25192f9cd538ded1c82e66a6ef3
Kevin Pierce <kevinpierce08@gmail.com>
2021-01-08 21:38:05 -0500

    Added product cards with image and product name

--- hype-4-less/src/components/PRODUCT-CARD/index.jsx
+++ hype-4-less/src/components/PRODUCT-CARD/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-const ProductCard = () => {
+const OuterCardDiv = styled.div`
+    border-radius:25px;
+    border:2px solid #000000;
+`
+const InnerCardDiv = styled.div`
+    margin:10px;
+`
+
+const CardProdImage = styled.img`
+    max-width:20%;
+    height:auto;
+`
+
+const ProductCard = (props) => {
     return (
-        <p>YUh</p>
+        <OuterCardDiv>
+            <InnerCardDiv>
+                <p>{props.prodName}</p>
+                <CardProdImage src={props.prodImg}></CardProdImage>
+            </InnerCardDiv>
+        </OuterCardDiv>
     );
 }
 
