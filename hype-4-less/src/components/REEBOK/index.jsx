afe2d39bd57601203d439013a62385e66d80c6ec
Kevin Pierce <kevinpierce08@gmail.com>
2021-01-09 17:27:58 -0500

    Added loader animation to Reebok sales page

--- hype-4-less/src/components/REEBOK/index.jsx
+++ hype-4-less/src/components/REEBOK/index.jsx
@@ -3,6 +3,52 @@ import styled from "styled-components";
 import axios from "axios";
 import ProductCard from "../PRODUCT-CARD/index";
 
+const StyledLoader = styled.div`
+&.loader,
+&.loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+&.loader {
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(93,76,76, 0.2);
+  border-right: 1.1em solid rgba(93,76,76, 0.2);
+  border-bottom: 1.1em solid rgba(93,76,76, 0.2);
+  border-left: 1.1em solid #5d4c4c;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+`
+
+
+
 const ProductsWrapper= styled.div`
     margin-left:20px;
     margin-right:20px;
@@ -34,7 +80,7 @@ const ReebokProdPage = () => {
     return (
         <div>
             {isLoading ? (
-                <h1>Is loading...</h1>
+                <StyledLoader className="loader">Loading...</StyledLoader>
             ) 
             : 
             (
